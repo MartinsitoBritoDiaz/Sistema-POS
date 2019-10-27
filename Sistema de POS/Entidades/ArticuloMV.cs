2b0007d79922af0d002066e6da64db4644bb13b8
Martinsito Brito Diaz <martinsito_brito@ucne.edu.do>
2019-10-27 18:13:16 -0400

    Registro cliente 50%

--- Sistema de POS/Entidades/ArticuloMV.cs
+++ Sistema de POS/Entidades/ArticuloMV.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace Sistema_de_POS.Entidades
 {
     public class ArticuloMV
     {
-        [key]
+        [Key]
         public int ArticuloMVid { get; set; }
         public string Descripcion { get; set; }
         public int code { get; set; }
